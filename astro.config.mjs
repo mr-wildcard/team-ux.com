03ddf24a65ed66c36a51a18ffd1f9c44d89d05ee
Julien Viala <julien.viala@shine.fr>
2023-09-12 23:09:59 +0200

    astro: site site config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
+/** @type {import('astro').AstroUserConfig} */
 export default defineConfig({
   integrations: [tailwind()],
+  site: "https://team-ux.com",
 });
