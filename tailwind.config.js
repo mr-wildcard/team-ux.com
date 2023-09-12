c41bfb5c63e3faa191f3e63da07689d91622c654
Julien Viala <julien.viala@shine.fr>
2023-09-12 22:49:58 +0200

    configure tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,29 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,ts}"],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ["SatoshiVariable", "sans-serif"],
+    extends: {
+      spacing: {
+        8: "30px",
       },
+    },
+    fontFamily: {
+      sans: ["SatoshiVariable", "sans-serif"],
+    },
+    colors: {
+      primary: "#0F24F7",
+      secondary: "#08124F",
+      surface1: "#F5F3EB",
+      surface2: "#FDF1D1",
+      surface3: "#FFD9A6",
+      surface4: "#FBBB64",
+      surface5: "#DCE1FF",
+      surface6: "#D3F5DE",
+      surface7: "#D3F5DE",
+      neutral1: "#474C6B",
+      neutral2: "#9DA1B5",
+      neutral3: "#DFE1EA",
+      neutral4: "#9DA1B5",
     },
   },
 };
