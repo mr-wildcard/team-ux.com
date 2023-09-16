77952a4d122041fcde9540cd2f1330beb2f4e529
Julien Viala <julien.viala@shine.fr>
2023-09-16 14:39:43 +0200

    support yml import

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+import yaml from "@rollup/plugin-yaml";
 
 /** @type {import('astro').AstroUserConfig} */
 export default defineConfig({
   integrations: [tailwind()],
   site: "https://team-ux.com",
+  vite: {
+    plugins: [yaml()],
+  },
 });
