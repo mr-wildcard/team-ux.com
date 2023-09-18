162c8ae297453acbbf63909d63f21b1d7279f1e0
Julien Viala <julien.viala@shine.fr>
2023-09-18 14:09:36 +0200

    chore: astro base conf for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,19 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import yaml from "@rollup/plugin-yaml";
 
-const builtForGithubPages = typeof process.env.GITHUB_ACTION !== undefined;
+const buildingForGithubPages = typeof process.env.GITHUB_ACTION !== undefined;
 
-const site = builtForGithubPages
+const site = buildingForGithubPages
   ? "https://mr-wildcard.github.io"
   : "htts://team-ux.com";
+
+const base = buildingForGithubPages ? "/team-ux.com" : "undefined";
 
 /** @type {import('astro').AstroUserConfig} */
 export default defineConfig({
   integrations: [tailwind()],
   site,
+  base,
   vite: {
     plugins: [yaml()],
   },
