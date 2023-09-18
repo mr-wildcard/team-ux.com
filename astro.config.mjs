2e66ab3105f0ed81e022b987d96ddaca35cff12d
Julien Viala <julien.viala@shine.fr>
2023-09-18 13:22:56 +0200

    chore: host website on pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,16 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import yaml from "@rollup/plugin-yaml";
 
+const builtForGithubPages = typeof process.env.GITHUB_ACTION !== undefined;
+
+const site = builtForGithubPages
+  ? "https://mr-wildcard.github.io"
+  : "htts://team-ux.com";
+
 /** @type {import('astro').AstroUserConfig} */
 export default defineConfig({
   integrations: [tailwind()],
-  site: "https://team-ux.com",
+  site,
   vite: {
     plugins: [yaml()],
   },
