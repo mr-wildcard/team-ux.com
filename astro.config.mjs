23684f36b2a2ed8070aad00a90b7e8009729352e
Julien Viala <julien.viala@shine.fr>
2023-09-18 14:45:06 +0200

    chore: specific base path for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,13 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import yaml from "@rollup/plugin-yaml";
 
-const buildingForGithubPages = typeof process.env.GITHUB_ACTION !== undefined;
+const buildingForGithubPages = process.env.GITHUB_ACTION !== undefined;
 
 const site = buildingForGithubPages
   ? "https://mr-wildcard.github.io"
-  : "htts://team-ux.com";
+  : "https://team-ux.com";
 
-const base = buildingForGithubPages ? "/team-ux.com" : "undefined";
+const base = buildingForGithubPages ? "/team-ux.com" : undefined;
 
 /** @type {import('astro').AstroUserConfig} */
 export default defineConfig({
