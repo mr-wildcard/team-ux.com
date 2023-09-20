635ef99231e197e312a2443bac99495bca7292af
Julien Viala <julien.viala@shine.fr>
2023-09-20 18:53:38 +0200

    feat: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,19 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import yaml from "@rollup/plugin-yaml";
-
+import sitemap from "@astrojs/sitemap";
 const buildingForGithubPages = process.env.GITHUB_ACTION !== undefined;
-
-const site = buildingForGithubPages
-  ? "https://mr-wildcard.github.io"
-  : "https://team-ux.com";
-
+const site = buildingForGithubPages ? "https://mr-wildcard.github.io" : "https://team-ux.com";
 const base = buildingForGithubPages ? "/team-ux.com" : undefined;
 
 /** @type {import('astro').AstroUserConfig} */
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
   site,
   base,
   vite: {
-    plugins: [yaml()],
-  },
+    plugins: [yaml()]
+  }
 });
