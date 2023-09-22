fc96f87dc83851fc08252b9ef47ecb84db2d7abf
Julien Viala <julien.viala@shine.fr>
2023-09-22 23:38:16 +0200

    chore: config website hosts

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,33 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import yaml from "@rollup/plugin-yaml";
 import sitemap from "@astrojs/sitemap";
-const buildingForGithubPages = process.env.GITHUB_ACTION !== undefined;
-const site = buildingForGithubPages ? "https://mr-wildcard.github.io" : "https://team-ux.com";
-const base = buildingForGithubPages ? "/team-ux.com" : undefined;
+
+const LOCAL_PORT = 4321;
+const LOCAL_HOST = "localhost";
+
+function getSiteURL() {
+  if (process.env.CF_PAGES) {
+    if (process.env.CF_PAGES_BRANCH !== "main") {
+      return `https://${process.env.CF_PAGES_BRANCH}.team-ux.pages.dev/`;
+    } else {
+      return "https://team-ux.com/";
+    }
+  } else {
+    return `http://${LOCAL_HOST}:${LOCAL_PORT}/`;
+  }
+}
 
 /** @type {import('astro').AstroUserConfig} */
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), sitemap()],
-  site,
-  base,
+  site: getSiteURL(),
   vite: {
-    plugins: [yaml()]
-  }
+    plugins: [yaml()],
+  },
+  server: {
+    host: LOCAL_HOST,
+    port: LOCAL_PORT,
+  },
 });
