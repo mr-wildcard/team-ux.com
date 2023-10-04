8cc973959f598f266f3321f162e0d661bdd06b01
Julien Viala <julien.viala@shine.fr>
2023-10-04 15:50:23 +0200

    aria-controls header submenu

---
+++ src/scripts/helpers/slugify.ts
@@ -0,0 +1,10 @@
+export function slugify(strings: string): string {
+  return strings
+    .toLowerCase()
+    .trim()
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .replace(/[^a-z0-9\s-]/g, " ")
+    .trim()
+    .replace(/[\s-]+/g, "-");
+}
