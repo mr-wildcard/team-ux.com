cb530fb440e25cf940541d5dfb130746b0b40c73
Julien Viala <julien.viala@shine.fr>
2023-10-17 22:13:35 +0200

    remove autoprefixer

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -3,9 +3,6 @@ module.exports = {
     require("postcss-import"),
     require("tailwindcss/nesting"),
     require("tailwindcss"),
-    require("autoprefixer")({
-      browsers: ["last 2 versions", "ie 6-8"],
-    }),
-    // require("cssnano"),
+    require("cssnano"),
   ],
 };
