f7e39dc61e929f59e204e6dffe46a482a7fe7c89
Julien Viala <julien.viala@shine.fr>
2024-09-11 21:52:09 +0200

    feat: 2024 disclaimer

--- astro.config.ts
+++ astro.config.ts
@@ -5,7 +5,7 @@ import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 
 const LOCAL_PORT = 4321;
-const LOCAL_HOST = "localhost";
+const LOCAL_HOST = "0.0.0.0";
 
 function getSiteURL() {
   if (process.env.CF_PAGES) {
