ea176557126c54b626c37cf7404ade91ab55ad8f
Julien Viala <julien.viala@shine.fr>
2025-02-10 13:59:58 +0100

    chore: move spacing 8 into conf file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,11 +2,4 @@ import { type Config } from "tailwindcss";
 
 export default {
   content: ["./src/**/*.{astro,ts}", "./public/**/*.html", "./public/**/*.css"],
-  theme: {
-    extend: {
-      spacing: {
-        8: "30px",
-      },
-    },
-  },
 } satisfies Config;
